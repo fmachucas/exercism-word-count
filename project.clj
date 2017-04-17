fa90a1c62e6bf31a291655cc2f73d3502f5dc066
Felipe Machuca <Felipe.Machuca@lan.com>
2017-04-17 16:24:06 -0300

    adds expectations and lein-test-refresh

--- project.clj
+++ project.clj
@@ -1,4 +1,6 @@
 (defproject word-count "0.1.0-SNAPSHOT"
   :description "word-count exercise."
   :url "https://github.com/exercism/xclojure/tree/master/exercises/word-count"
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [expectations "2.2.0-beta1"]]
+  :profiles {:dev {:plugins [[com.jakemccrary/lein-test-refresh "0.19.0"]]}})
