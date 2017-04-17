934cbe7a4aa2b0c0460f43a3ea250835c812ceeb
Felipe Machuca <Felipe.Machuca@lan.com>
2017-04-17 16:24:48 -0300

    uses expectations syntaxis

--- test/word_count_test.clj
+++ test/word_count_test.clj
@@ -1,27 +1,28 @@
 (ns word-count-test
-  (:require [clojure.test :refer [deftest is]]
+(:require [expectations :refer :all]
+          [expectations.clojure.test :refer [defexpect]]
             word-count))
 
-(deftest count-one-word
-  (is (= {"word" 1}
-         (word-count/word-count "word"))))
+(defexpect count-one-word
+  (expect {"word" 1}
+         (word-count/word-count "word")))
 
-(deftest count-one-of-each
-  (is (= {"one" 1 "of" 1 "each" 1}
-         (word-count/word-count "one of each"))))
+(defexpect count-one-of-each
+  (expect {"one" 1 "of" 1 "each" 1}
+         (word-count/word-count "one of each")))
 
-(deftest count-multiple-occurrences
-  (is (= {"one" 1 "fish" 4 "two" 1 "red" 1 "blue" 1}
-         (word-count/word-count "one fish two fish red fish blue fish"))))
+(defexpect count-multiple-occurrences
+  (expect {"one" 1 "fexpecth" 4 "two" 1 "red" 1 "blue" 1}
+         (word-count/word-count "one fexpecth two fexpecth red fexpecth blue fexpecth")))
 
-(deftest ignore-punctuation
-  (is (= {"car" 1, "carpet" 1 "as" 1 "java" 1 "javascript" 1}
-         (word-count/word-count "car : carpet as java : javascript!!&@$%^&"))))
+(defexpect ignore-punctuation
+  (expect {"car" 1, "carpet" 1 "as" 1 "java" 1 "javascript" 1}
+         (word-count/word-count "car : carpet as java : javascript!!&@$%^&")))
 
-(deftest include-numbers
-  (is (= {"testing" 2 "1" 1 "2" 1}
-         (word-count/word-count "testing, 1, 2 testing"))))
+(defexpect include-numbers
+  (expect {"testing" 2 "1" 1 "2" 1}
+         (word-count/word-count "testing, 1, 2 testing")))
 
-(deftest normalize-case
-  (is (= {"go" 3}
-         (word-count/word-count "go Go GO"))))
+(defexpect normalize-case
+  (expect {"go" 3}
+         (word-count/word-count "go Go GO")))
